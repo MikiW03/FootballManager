505eb621ae7dcdbb293053da22e7c7d12cf64e37
MikiW03 <mikiwiniecki@gmail.com>
2024-06-02 00:28:31 +0200

    Create console interface

--- League.cs
+++ League.cs
@@ -1,4 +1,4 @@
-namespace FootballManager;
+﻿namespace FootballManager;
 
 public class League(Dictionary<string, Team> teams, List<Round> rounds, ISavable dataSaver)
 {
@@ -10,8 +10,36 @@ public class League(Dictionary<string, Team> teams, List<Round> rounds, ISavable
     {
         foreach (var round in Rounds)
         {
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine($"Round {Rounds.IndexOf(round) + 1}");
+            Console.WriteLine("-----------------------------------------");
+
             round.Simulate();
             DataSaver.SaveData(round, Rounds.IndexOf(round) + 1);
+
+            Console.WriteLine();
+            Console.WriteLine($"Table after round {Rounds.IndexOf(round) + 1}:");
+
+            Teams = Teams.OrderByDescending(x => x.Value.Points)
+                        .ThenByDescending(x => x.Value.GoalsScored - x.Value.GoalsConceded)
+                        .ToDictionary();
+            PrintTable();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+
+    private void PrintTable()
+    {
+        const string formattingString = "{8,3} {0,-25} {1,-8} {2,-3} {3,-3} {4,-3} {5,-4} {6,-4} {7,-10}";
+        Console.WriteLine(formattingString, "Team Name", "Matches", "W", "D", "L", "GS", "GC", "Points", "No");
+        foreach (var (teamName, team) in Teams)
+        {
+            if (teamName == "Manchester City") Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(formattingString, teamName, team.MatchesPlayed, team.Wins, team.Draws, team.Losses, team.GoalsScored, team.GoalsConceded, team.Points, Teams.Values.ToList().IndexOf(team) + 1);
+            Console.ResetColor();
         }
     }
 }
--- Round.cs
+++ Round.cs
@@ -1,4 +1,7 @@
-﻿namespace FootballManager;
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FootballManager;
 
 public class Round
 {
@@ -9,6 +12,50 @@ public class Round
         foreach (var match in Matches)
         {
             match.Simulate();
+            Console.WriteLine("{0,-25} {3,-25} {1,2}:{2,-2}", match.HomeTeam.Name, match.HomeGoals, match.AwayGoals, match.AwayTeam.Name);
+
+            if (Matches.IndexOf(match) != 0) continue;
+            Console.WriteLine();
+            Console.WriteLine($"{match.HomeTeam.Name}'s lineup: ");
+            PrintLineup(match.HomeLineup, match.HomeTeam);
+
+            Console.WriteLine($"{match.AwayTeam.Name}'s lineup: ");
+            PrintLineup(match.AwayLineup, match.AwayTeam);
+
+            Console.WriteLine("Match events:");
+            match.Events.OrderBy(eventItem => eventItem.Minute).ToList().ForEach(eventItem => eventItem.PrintDetails());
+
+            Console.WriteLine();
+            Console.WriteLine("Other matches: ");
         }
+    }
+
+    private void PrintLineup(List<Player> lineup, Team team)
+    {
+        var playerIndex = 1;
+        var defence = "";
+        for (var i = 0; i < team.Formation.Defenders; i++)
+        {
+            defence += $"{lineup[playerIndex].Name}({lineup[playerIndex++].Overall}) - ";
+        }
+        defence = defence[..^3];
+        var midfield = "";
+        for (var i = 0; i < team.Formation.Midfielders; i++)
+        {
+            midfield += $"{lineup[playerIndex].Name}({lineup[playerIndex++].Overall}) - ";
+        }
+        midfield = midfield[..^3];
+        var attack = "";
+        for (var i = 0; i < team.Formation.Forwards; i++)
+        {
+            attack += $"{lineup[playerIndex].Name}({lineup[playerIndex++].Overall}) - ";
+        }
+        attack = attack[..^3];
+
+        Console.WriteLine($"Goalkeeper: {lineup[0].Name}({lineup[0].Overall})");
+        Console.WriteLine($"Defence: {defence}");
+        Console.WriteLine($"Midfield: {midfield}");
+        Console.WriteLine($"Attack: {attack}");
+        Console.WriteLine();
     }
 }
